0fa15daad6707582eb7cfb1e441422edef1924a1
rzi <rzi@vp.pl>
2019-03-31 22:48:04 +0200

    pi 31/03

---
+++ index.php
@@ -0,0 +1,35 @@
+﻿<html>
+<head>
+<title>Pomiar temperatury Raspberry PI</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!-- <meta http-equiv="content-type" content="text/html; charset=iso-8859-2"> -->
+</head>
+<body>
+
+<h2 align="center">Pomiar temperatury i zapis w bazie danych</h2>
+<h2 align="left">Opis projektu.</h2>
+<p>Celem projektu są pomiary temperatur, ich
+zapis do zewnętrznej bazy danych oraz sterowanie zaworami ogrzewania podłogowego</p>
+<img src="schemat_rpi.png"> <br>
+<h3>1.Hardware:</h3>
+<p>Czujnik temperatury DS18B20<br>
+Moduł interfejsu One wire na USB firmy Meraprojekt (FT232RL + DS2480)<br>
+Raspberry Pi jako serwer<br>
+Moduł Piface<br>
+Moduł 8 przekaźników </p>
+<h3>2.Software:</h3>
+<p>Serwer HTTP (Apache, PHP, MySQL) <br>
+Python - oprogramowanie modułu pomiarowego<br>
+<h2>Pomiar temperatury- Monitor danych (archiwum)</h2>
+<a href="new_wyk5.php">Pomiar temperatury - wykresy >>></a><br>
+<a href="new_cz.php">Pomiar temperatury - dane >>></a><br><br>
+<?php
+session_start();
+if($_GET["wyloguj"]=="tak"){$_SESSION["zalogowany"]=0;echo "Zostals wylogowany z serwisu";}
+
+echo $_SERVER['HTTP_USER_AGENT'];
+
+?>
+
+</body>
+</html>
---
+++ login2.php
@@ -0,0 +1,69 @@
+﻿ <?php header('Content-Type: text/html; charset=utf-8'); ?>
+<html
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!-- <meta http-equiv="content-type" content="text/html; charset=iso-8859-2"> -->
+<title>Strona główna logowania</title>
+</head>
+<body>
+<?
+session_start();
+//session_register("zalogowany");
+if(empty($_SESSION["zalogowany"]))$_SESSION["zalogowany"]=0;
+
+
+
+if($_GET["wyloguj"]=="tak"){$_SESSION["zalogowany"]=0;echo "Zostałeś wylogowany z serwisu";}
+if($_SESSION["zalogowany"]!=1){
+	if(!empty($_POST["login"]) && !empty($_POST["haslo"])){
+		require "connection1.php";
+		connection1();
+		if(mysql_num_rows(mysql_query("select * from konta where login = '".htmlspecialchars($_POST["login"])."' AND haslo = '".htmlspecialchars($_POST["haslo"])."'"))){
+			//echo "Zalogowano poprawnie. <a href='dane_temp.php'>Przejdź na stronę główną</a>";
+			$_SESSION["zalogowany"]=1;
+			header( 'Location: tab1.php' ) ;
+			}
+		else echo ShowLogin("Podano złe dane!!!");
+		}
+
+	else {
+	echo "<br>";?>
+
+	<table border = "0">
+
+	<tr >
+	<td valign="bottom" align="center" style="width: 160px" style="height: 40px"></td>
+	<td bgcolor="yellow" valign="bottom" align="center" style="width: 340px" style="height: 40px"> <? echo " Logowanie, wprowadź poprawne dane:"?></td>
+	<td valign="bottom" align="center" style="width: 160px" style="height: 40px"></td>
+	</tr>
+	<tr >
+	<td valign="bottom" align="center" style="width: 160px" style="height: 40px"></td>
+	<td bgcolor="yellow" valign="bottom" align="center" style="width: 360px" style="height: 40px"><?ShowLogin();}?></td>
+	<td valign="bottom" align="center" style="width: 160px" style="height: 40px"></td>
+	</tr>
+	</table>
+	<?
+
+	//ShowLogin();}
+}
+else{
+header( 'Location: tab1.php' ) ;
+
+}
+?>
+
+
+</body>
+</html>
+<?php mysql_close(); ?>
+
+<?php
+function ShowLogin($komunikat=""){
+	echo "$komunikat<br>";
+	echo "<form action='login2.php' method=post>";
+	echo "Login: <input type=text name=login><br>";
+	echo "Hasło: <input type=password name=haslo><br>";
+	echo "<input type=submit value='Zaloguj!'>";
+	echo "</form>";
+}
+?>
