e28cd799a42776bfacc465d42862007727da749d
rzi <rzi@vp.pl>
2019-04-14 20:29:06 +0200

    14/04

--- new_wyk5.php
+++ new_wyk5.php
@@ -12,6 +12,13 @@
             crossorigin="anonymous"></script>
 
     <script type="text/javascript" src="/pi/js/skrypt.js"></script>
+  <?php
+  header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+  header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
+  header("Connection: close");
+  ?>
 </head>
 <body>
     <h1>Pomiary temperatur - wykresy </h1>
