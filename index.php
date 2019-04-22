237d151bc0be5d4c50baa645ac7403f32b929871
rzi <rzi@vp.pl>
2019-04-22 14:03:07 +0200

    22/04

--- index.php
+++ index.php
@@ -7,21 +7,24 @@
 <link rel="stylesheet" href="/pi/css/style.css" type="text/css" />
 </head>
 <body>
-<h1 align="center">Pomiar temperatury i zapis w bazie danych</h1>
-<h2 align="left">Opis projektu.</h2>
-<p>Celem projektu są pomiary temperatur, ich
-zapis do zewnętrznej bazy danych oraz sterowanie zaworami ogrzewania podłogowego</p>
-<img src="/pi/img/schemat_rpi.png"> <br>
-<h3>1.Hardware:</h3>
-<p>Czujnik temperatury DS18B20<br>
-Moduł interfejsu One wire na USB firmy Meraprojekt (FT232RL + DS2480)<br>
-Raspberry Pi jako serwer<br>
-Moduł 8 przekaźników </p>
-<h3>2.Software:</h3>
-<p>Serwer HTTP (Apache, PHP, MySQL) <br>
-Python - oprogramowanie modułu pomiarowego<br>
-<h2>Pomiar temperatury- Monitor danych (archiwum)</h2>
-<a href="new_wyk5.php">Pomiar temperatury - wykresy >>></a><br>
-<a href="new_cz.php">Pomiar temperatury - dane >>></a><br>
+<div>
+  <h1 align="center">Pomiar temperatury i zapis w bazie danych</h1>
+  <h2 align="left">Opis projektu.</h2>
+  <p>Celem projektu są pomiary temperatur, ich
+    zapis do zewnętrznej bazy danych oraz sterowanie zaworami ogrzewania podłogowego</p>
+  <img src="/pi/img/schemat_rpi.png"> <br>
+  <h3>1.Hardware:</h3>
+  <p>Czujnik temperatury DS18B20<br>
+    Moduł interfejsu One wire na USB firmy Meraprojekt (FT232RL + DS2480)<br>
+    Raspberry Pi jako serwer<br>
+    Moduł 8 przekaźników </p>
+  <h3>2.Software:</h3>
+  <p>Serwer HTTP (Apache, PHP, MySQL) <br>
+    Python - oprogramowanie modułu pomiarowego<br>
+  <h2>Pomiar temperatury- Monitor danych (archiwum)</h2>
+  <a href="new_wyk5.php">Pomiar temperatury - wykresy >>></a><br>
+  <a href="new_cz.php">Pomiar temperatury - dane >>></a><br>
+</div>
+
 </body>
 </html>
