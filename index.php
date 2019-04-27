0b1660b155e78e4b724a256c4db3fa7db804686c
Rafal Zietak <rzi@vp.pl>
2019-04-27 21:08:16 +0200

    27/04

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <title>Pomiar temperatury Raspberry PI</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/normalize.css" type="text/css" />
 <link rel="stylesheet" href="/css/style.css" type="text/css" />
 </head>
 <body>
