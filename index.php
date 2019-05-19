6bebf15ee83a98e438a162987fd9bbe3f5cc4eae
Rafal Zietak <rzi@vp.pl>
2019-05-19 18:08:16 +0200

    19/05

--- index.php
+++ index.php
@@ -27,9 +27,8 @@
     <h3>Pomiar temperatury- Monitor danych (archiwum)</h3>
     <a href="new_wyk5.php">Pomiar temperatury - wykresy >>></a><br/>
     <a href="new_cz.php">Pomiar temperatury - dane >>></a>
+  </div>
 </div>
-</div>
-
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
