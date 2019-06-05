0e592e44b870b195ffdc1409db668829b842dfb6
Rafal Zietak <rzi@vp.pl>
2019-06-05 20:04:00 +0200

    unindex list

--- index.php
+++ index.php
@@ -17,17 +17,43 @@
       zapis do zewnętrznej bazy danych oraz sterowanie zaworami ogrzewania podłogowego</p>
       <img alt="Pi" src="/img/schemat_rpi2.png" class="img-fluid"> <br>
     <h4>1.Hardware:</h4>
-    <p>Czujnik temperatury DS18B20<br>
-      Moduł interfejsu One wire na USB firmy Meraprojekt (FT232RL + DS2480)<br>
-      Raspberry Pi jako serwer<br>
-      Moduł 8 przekaźników </p>
+    <ul>
+      <li>
+      Czujnik temperatury DS18B20
+      </li>
+      <Li>
+      Moduł interfejsu One wire na USB firmy Meraprojekt (FT232RL + DS2480)
+      </Li>
+      <li>
+      Raspberry Pi jako serwer
+      </li>
+      <li>
+      Moduł 8 przekaźników 
+      </li>
+    </ul>  
+   
     <h4>2.Software:</h4>
-    <p>Serwer HTTP (Apache, PHP, MySQL) <br>
-      Python - oprogramowanie modułu pomiarowego<br>
+    <ul>
+      <li>
+      Serwer HTTP (Apache, PHP, MySQL)
+      </li>
+      <li>
+      Python - oprogramowanie modułu pomiarowego
+      </li>
+    </UL>
+    
     <h3>Pomiar temperatury- Monitor danych (archiwum)</h3>
-    <a href="new_wyk5.php">Pomiar temperatury - wykresy >>></a><br/>
-    <a href="new_cz.php">Pomiar temperatury - dane >>></a>
-    <br/>
+    <ul>
+    
+      <li>
+      <a href="new_wyk5.php">Pomiar temperatury - wykresy >>></a>
+      </li>
+      
+      <li>
+      <a href="new_cz.php">Pomiar temperatury - dane >>></a>
+      </li>
+    </ul>
+    
   </div>
 </div>
 <!-- Optional JavaScript -->
